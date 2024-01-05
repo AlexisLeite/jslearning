f88d5e8f25aa70093bf29136431ece70d4063909
Eduardo Pérez <justedu43@gmail.com>
2024-01-04 21:16:14 -0300

    dale

--- src/eduardo/list.v02/components/Prueba1.jsx
+++ src/eduardo/list.v02/components/Prueba1.jsx
@@ -6,6 +6,21 @@ const styles = {
 
   border: '1px solid',
 };
+/*export const Pruebas1 = () => {
+  const [texto, setTexto] = useState('hola');
+  const cambiarTexto = () => {
+    const nuevoTexto =  texto === 'hola' ?  'chau': 'hola';
+    setTexto(nuevoTexto);
+  };
+  return (
+    <Box sx={styles}>
+      {' '}
+      {texto}
+      <Button onClick={cambiarTexto}></Button>
+    </Box>
+  );
+};
+*/
 export const Pruebas1 = () => {
   const [texto, setTexto] = useState('hola');
 
@@ -15,11 +30,9 @@ export const Pruebas1 = () => {
       {texto}
       <Button
         onClick={() => {
-          setTexto('chau ');
+          setTexto('chau');
         }}
-      >
-        CLick
-      </Button>
+      ></Button>
     </Box>
   );
 };
